7de945242fbbff82b54035aa7aefcf40437e021d
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-06-30 20:46:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const modulink = require('./modulink.js')
-modulink('https://pastebin.com/raw/RGe3D7HW')/*.then(e => {
+modulink('https://raw.githubusercontent.com/FabioSmuu/modulink/master/exemplo/script.js')
+
+/*modulink('https://pastebin.com/raw/RGe3D7HW').then(e => {
 	console.log(e)
 }).catch(e => {
 	console.log(e)
